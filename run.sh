11c7143033eee3a28c4d373490bc858b018c43ba
Damith C. Rajapakse <damith@gmail.com>
2024-03-20 23:25:00 +0800

    run.sh: Use master

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-./get-reposense.py --commit 22992cbc1a89bcb115cfb9fc169a5d68a9682550
+./get-reposense.py --master
 java -jar RepoSense.jar --config ./configs --since 23/02/2024  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
